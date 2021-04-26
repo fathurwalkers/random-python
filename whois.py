4329c109116c39b37432eeeefdc9ff94f63f587d
Fathur Walkers <fathurwalkers44@gmail.com>
2021-04-26 13:07:56 -0700

    update

--- whois.py
+++ whois.py
@@ -12,24 +12,21 @@ sess = requests.Session()
 src_request = input('Masukkan Request : ')
 url = sess.get('https://www.hostinger.co.id/api/domain-search', headers={'User-Agent': ua.chrome}, allow_redirects=False)
 payload = {"domain":src_request}
+
 def doRequest():
     source = 'https://www.hostinger.co.id/whois'
     getPost = requests.post(source, json=payload).text
-    
     formatN = getPost.replace("\\n", "\n")
     formatstrip = formatN.replace("\/\/", "//")
     formatstripsecond = formatN.replace("\/", "/")
-    
     print(formatstripsecond)
     
 def doRequestToArray():
     source = 'https://www.hostinger.co.id/whois'
     getPost = requests.post(source, json=payload).text
-    
     formatN = getPost.replace("\\n", "^^")
     formatstrip = formatN.replace("\/\/", "//")
     formatstripsecond = formatN.replace("\/", "/")
-    
     formatsplit = formatstripsecond.split("^^")
     
     print(formatsplit)
